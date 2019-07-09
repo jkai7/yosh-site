19dba57dee48e50c6c0158e29755a17be820b7bf
Jens Kai <jkaih7@gmail.com>
2019-07-09 11:59:22 -0400

    added favicon and typed js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@ $(document).ready(function(){
 
     $('#slides').superslides({
         animation: 'fade',
-        play: 5000
+        play: 5000,
+        pagination: false
     });
 
-
+    var typed = new Typed(".typed", {
+        strings: ["I'm a Badass Lawyer", "Immigration Master", "Overall Cool Guy"],
+        typeSpeed: 80,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
 
 
 
