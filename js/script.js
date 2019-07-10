dca415d9ca472761947c84ca9a83a93f0205aa41
Jens Kai <jkaih7@gmail.com>
2019-07-10 02:34:22 -0400

    finished sticky nav

--- js/script.js
+++ js/script.js
@@ -15,6 +15,22 @@ $(document).ready(function(){
     });
 
 
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+        const body = $("body");
+
+        if($(window).scrollTop() >= navTop) {
+            body.css("padding-top", nav.outerHeight() + "px");
+            body.addClass("fixedNav");
+        }else{
+            body.css("padding-top", 0);
+            body.removeClass("fixedNav");
+        }
+    }
 
 
 });
