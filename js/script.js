87912f32a1dd4648354a851a535a2d9e21ed0d93
Jens Kai <jkaih7@gmail.com>
2019-07-10 20:10:13 -0400

    uploaded real pics

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typed", {
-        strings: ["I'm a Badass Lawyer", "Immigration Master", "Overall Cool Guy"],
+        strings: ["Immigration Law", "Sports Law", "International Arbitration", "Media and Entertainment Law", "Intellectual Property", "Corporate and Commercial"],
         typeSpeed: 80,
         loop: true,
         startDelay: 1000,
